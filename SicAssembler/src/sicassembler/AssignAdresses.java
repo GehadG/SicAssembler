f7e67e2929c5e1a96532608e42af1a9aaab84a34
Mahmoud <Mahmoud@Mahmoud-PC>
2015-11-28 00:47:39 +0200

    update 12:47 AM RESB RESW

--- SicAssembler/src/sicassembler/AssignAdresses.java
+++ SicAssembler/src/sicassembler/AssignAdresses.java
@@ -23,18 +23,41 @@ public class AssignAdresses {
         long decimal = Long.parseLong(initial, 16);
         System.out.println(decimal);
         for (int i=1;i<y.size();i++){
-            if(y.get(i).getOperation().toLowerCase().equals("byte"))
+            if(y.get(i).getOperation().toLowerCase().equalsIgnoreCase("byte"))
             {
-            String hex = Long.toHexString(decimal);
-            y.get(i).setAddress(hex);
-                decimal=decimal+y.get(i).getOperand().length()-3;
+                String m = y.get(i).getOperand();
+                String n = m.substring(0, 1);
+                if (n.equalsIgnoreCase("x")){
+                    String hex = Long.toHexString(decimal);
+                    y.get(i).setAddress(hex);
+                    decimal = decimal +1;
+                }
                 
+                else{
+                     String hex = Long.toHexString(decimal);
+                     y.get(i).setAddress(hex);
+                     decimal=decimal+y.get(i).getOperand().length()-3;
+                }
+            }
+            else if (y.get(i).getOperation().toLowerCase().equalsIgnoreCase("resw")){
+                String k = y.get(i).getOperand();
+                int k1 = Integer.parseInt(k);
+                String hex = Long.toHexString(decimal);
+                y.get(i).setAddress(hex);
+                decimal = decimal +(3*k1);
+            }
+            else if(y.get(i).getOperation().toLowerCase().equalsIgnoreCase("resb")){
+                String h = y.get(i).getOperand();
+                int h1 = Integer.parseInt(h);
+                String hex = Long.toHexString(decimal);
+                y.get(i).setAddress(hex);
+                decimal = decimal +(1*h1);
             }
             else
             {
                 String hex = Long.toHexString(decimal);
-            y.get(i).setAddress(hex);
-            decimal = decimal +3;
+                y.get(i).setAddress(hex);
+                decimal = decimal +3;
             
                     }
         }
