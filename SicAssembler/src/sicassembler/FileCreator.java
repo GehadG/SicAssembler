279ff45e53af322c02b0c952b1ece9e1848e89f4
user <user@user-PC.lan>
2015-11-30 04:58:48 +0200

    Fixed Object File

--- SicAssembler/src/sicassembler/FileCreator.java
+++ SicAssembler/src/sicassembler/FileCreator.java
@@ -24,11 +24,10 @@ public class FileCreator {
     public FileCreator() {
         l2 = ObjectCode.objectcode();
         createLIS();
-       createOBJ();
+        createOBJ();
     }
 
     private void createLIS() {
-        
 
         File file = new File("LISFILE");
         FileWriter fw;
@@ -50,17 +49,15 @@ public class FileCreator {
         }
 
     }
-    
-    private void createOBJ()
-    {
+
+    private void createOBJ() {
         File file = new File("DEVF2");
         FileWriter fw;
-        
-        
+
         try {
-            String head=getHead();
-            String text=getText();
-            String end=getEnd();
+            String head = getHead();
+            String text = getText();
+            String end = getEnd();
             fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write(head);
@@ -74,84 +71,100 @@ public class FileCreator {
             Logger.getLogger(SicAssembler.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        
     }
-private String hexCal(String a,String b){
-    int sa,ea;
-    sa = Integer.parseInt(a,16);
-    ea = Integer.parseInt(b,16);
-    String hex=Integer.toHexString(ea-sa);
-     hex=hex.toUpperCase();
-     hex=String.format("%2s",hex).replace(' ', '0');
-    return hex;
-}
-private String getHead(){
-    int i;
-            String hex,fi  =  l2.get(0).getLabel();
-            String add=l2.get(0).getAddress();
-           if(fi.length()<6){
-               i=6-fi.length();
-               fi=String.format("%-6s",fi);
-           }
-           if(add.length()<6)
-           {
-                add=String.format("%6s",add).replace(' ', '0');
-           }
-           hex=hexCal(l2.get(0).getAddress(),l2.get(l2.size()-1).getAddress());
-           if(hex.length()<6){
-               i=6-fi.length();
-               hex=String.format("%6s",hex).replace(' ', '0');
-           }
-return "H"+fi+add+hex;
-}
-private String getText(){
-    int max=10,i=1;
-    String objcode="";
-    String len,end=String.format("%6s",l2.get(1).getAddress()).replace(' ', '0'),start=String.format("%6s",l2.get(1).getAddress()).replace(' ', '0');
-    String text="";
-    for(i=1;i<=l2.size()-1;i++){
-        if(l2.get(i).getComment().equalsIgnoreCase("No Comment")==false)
-        {//Matshelsh Dyh 3ashan deeh betcheck el instruction dyh comment walla l2a .
-            continue;
+
+    private String hexCal(String a, String b) {
+        int sa, ea;
+        sa = Integer.parseInt(a, 16);
+        ea = Integer.parseInt(b, 16);
+        String hex = Integer.toHexString(ea - sa);
+        hex = hex.toUpperCase();
+        hex = String.format("%2s", hex).replace(' ', '0');
+        return hex;
+    }
+
+    private String getHead() {
+        int i;
+        String hex, fi = l2.get(0).getLabel();
+        String add = l2.get(0).getAddress();
+        if (fi.length() < 6) {
+            i = 6 - fi.length();
+            fi = String.format("%-6s", fi);
         }
-        if(text==""){
-            text="T"; 
+        if (add.length() < 6) {
+            add = String.format("%6s", add).replace(' ', '0');
         }
-        else if(text.charAt(text.length()-1)=='T'){
-            text=text+end;
+        hex = hexCal(l2.get(0).getAddress(), l2.get(l2.size() - 1).getAddress());
+        if (hex.length() < 6) {
+            i = 6 - fi.length();
+            hex = String.format("%6s", hex).replace(' ', '0');
         }
-        if(l2.get(i).getOperation().equals("resw")||l2.get(i).getOperation().equals("resb")){
-           end=String.format("%6s",l2.get(i).getAddress()).replace(' ', '0');
-            len=hexCal(start,end);
-            end=String.format("%6s",l2.get(i+1).getAddress()).replace(' ', '0');
-            start=String.format("%6s",l2.get(i+1).getAddress()).replace(' ', '0');
-            text=text+len+objcode+"\r\nT";
-            objcode="";
-        }
-        else if(objcode.length()<60)
-        {
-            objcode=objcode+l2.get(i).getObjcode();
-            objcode=objcode.toUpperCase();
-            if(l2.get(i).getOperation().equals("end")){
-                end=String.format("%6s",l2.get(i).getAddress()).replace(' ', '0');
-            len=hexCal(start,end);
-            start=String.format("%6s",l2.get(i).getAddress()).replace(' ', '0');
-            text=text+len+objcode;
+        return "H" + fi + add + hex;
+    }
+
+    private String getText() {
+        int max = 10, i = 1;
+        String objcode = "";
+        String len, end = String.format("%6s", l2.get(1).getAddress()).replace(' ', '0'), start = String.format("%6s", l2.get(1).getAddress()).replace(' ', '0');
+        String text = "";
+        for (i = 1; i <= l2.size() - 1; i++) {
+            if (l2.get(i).getComment().equalsIgnoreCase("No Comment") == false) {//Matshelsh Dyh 3ashan deeh betcheck el instruction dyh comment walla l2a .
+                continue;
+            }
+            if (text == "") {
+                text = "T";
+            }
+            if (text.charAt(text.length() - 1) == 'T') {
+                text = text + end;
+            }
+            if (l2.get(i).getOperation().equalsIgnoreCase("end")) {
+                end = String.format("%6s", l2.get(i).getAddress()).replace(' ', '0');
+                len = hexCal(start, end);
+                start = String.format("%6s", l2.get(i).getAddress()).replace(' ', '0');
+                text = text + len + objcode;
+            } else if (objcode.length() == 60) {
+                end = String.format("%6s", l2.get(i).getAddress()).replace(' ', '0');
+                len = hexCal(start, end);
+                start = String.format("%6s", l2.get(i).getAddress()).replace(' ', '0');
+                text = text + len + objcode + "\r\nT";
+                objcode = l2.get(i).getObjcode();
+            } else if (objcode.length() < 60) {
+                if (l2.get(i).getOperation().equalsIgnoreCase("byte") || l2.get(i).getOperation().equalsIgnoreCase("word")) {
+                    String x = objcode;
+                    objcode = objcode + l2.get(i).getObjcode();
+                    if (objcode.length() > 60) {
+                        objcode = x;
+                        end = String.format("%6s", l2.get(i).getAddress()).replace(' ', '0');
+                        len = hexCal(start, end);
+                        start = String.format("%6s", l2.get(i).getAddress()).replace(' ', '0');
+                        text = text + len + objcode + "\r\nT";
+                        objcode = l2.get(i).getObjcode();
+                    }
+                } else if (!l2.get(i).getOperation().equalsIgnoreCase("resw") && !l2.get(i).getOperation().equalsIgnoreCase("resb")) {
+                    objcode = objcode + l2.get(i).getObjcode();
+                    objcode = objcode.toUpperCase();
+                } else if (l2.get(i).getOperation().equalsIgnoreCase("resw") || l2.get(i).getOperation().equalsIgnoreCase("resb")) {
+                    if (l2.get(i + 1).getOperation().equalsIgnoreCase("resw") || l2.get(i + 1).getOperation().equalsIgnoreCase("resw")) {
+                        continue;
+                    } else if (l2.get(i + 1).getOperation().equalsIgnoreCase("end")) {
+                        continue;
+                    }
+                    end = String.format("%6s", l2.get(i).getAddress()).replace(' ', '0');
+                    len = hexCal(start, end);
+                    end = String.format("%6s", l2.get(i + 1).getAddress()).replace(' ', '0');
+                    start = String.format("%6s", l2.get(i + 1).getAddress()).replace(' ', '0');
+                    text = text + len + objcode + "\r\nT";
+                    objcode = "";
+                }
             }
         }
-        else if(objcode.length()==60){
-            end=String.format("%6s",l2.get(i).getAddress()).replace(' ', '0');
-            len=hexCal(start,end);
-            start=String.format("%6s",l2.get(i).getAddress()).replace(' ', '0');
-            text=text+len+objcode+"\r\nT";
-            objcode=l2.get(i).getObjcode();
-        }
+        return text;
     }
-return text;
-}
-private String getEnd(){
-    String text=String.format("%6s",l2.get(0).getAddress()).replace(' ', '0');;
-    text="E"+text;
-    return text;
-}
+
+    private String getEnd() {
+        String text = String.format("%6s", l2.get(0).getAddress()).replace(' ', '0');;
+        text = "E" + text;
+        return text;
+    }
+
 }
